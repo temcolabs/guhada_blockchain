9f9d413753f4d40b13bc181c13b8162b181c2199
ShinHan <shin.han@temco.io>
2019-02-21 18:25:59 +0900

    불필요한 코드 제거

--- src/test/java/io/temco/guhada/blockchain/BlockchainApplicationTests.java
+++ src/test/java/io/temco/guhada/blockchain/BlockchainApplicationTests.java
@@ -1,6 +1,5 @@
 package io.temco.guhada.blockchain;
 
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,18 +11,18 @@ public class BlockchainApplicationTests {
 
     @Test
     public void contextLoads() {
-        System.setProperty("jasypt.encryptor.password", "t3mco@dminUser");
-
-        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
-        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
-        pbeEnc.setPassword("t3mco@dminUser"); //2번 설정의 암호화 키를 입력
-
-        String enc = pbeEnc.encrypt("devTemco88!"); //암호화 할 내용
-        System.out.println("enc = " + enc); //암호화 한 내용을 출력
-
-        //테스트용 복호화
-        String des = pbeEnc.decrypt(enc);
-        System.out.println("des = " + des);
+//        System.setProperty("jasypt.encryptor.password", "t3mco@dminUser");
+//
+//        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+//        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+//        pbeEnc.setPassword("t3mco@dminUser"); //2번 설정의 암호화 키를 입력
+//
+//        String enc = pbeEnc.encrypt("devTemco88!"); //암호화 할 내용
+//        System.out.println("enc = " + enc); //암호화 한 내용을 출력
+//
+//        //테스트용 복호화
+//        String des = pbeEnc.decrypt(enc);
+//        System.out.println("des = " + des);
 
     }
 
